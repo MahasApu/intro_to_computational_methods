6a8ea0bd934b23dceffd489cd8bf568ea7dd6e0e
MahasApu <maryvask0@yandex.ru>
2024-04-26 17:59:32 +0700

    add estimation of an order of approx for 4th task

--- tasks/integration/simpson_integration.py
+++ tasks/integration/simpson_integration.py
@@ -1,12 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+
 from math import sin, exp, sqrt, pi, inf, log
-from scipy import integrate
 from abc import abstractmethod, ABCMeta
 from typing import Callable
 
 ACCURACY = 10 ** -8
 
 FIRST_TEST = 1
-SECOND_TEST = 0
+SECOND_TEST = 1
+THIRD_TEST = 1
 
 
 class Test:
@@ -21,30 +25,34 @@ class Test:
 
 
 class FirstTest(Test):
-    bounds: tuple[float, float] = (0, inf)
+    nodes_amount = 10000
+    bounds: tuple[float, float] = (0, 1)
 
     def expression(self) -> Callable:
         return lambda x: pi if x == 0 else pi * 5 if x == 1 else sin(pi * x ** 5) / ((1 - x) * x ** 5)
 
 
 class SecondTest(Test):
-    bounds: tuple[float, float] = (0, 1)
+    nodes_amount = 10000
+    bounds: tuple[float, float] = (0, inf)
 
     def expression(self) -> Callable:
         return lambda x: exp(-sqrt(x) + sin(x / 10))
 
 
-""" 
-The approximation order of the composite formula is 4
-The algebraic order is 3
-"""
+class ThirdTest(Test):
+    nodes_amount = 1000
+    bounds: tuple[float, float] = (0, pi)
+
+    def expression(self) -> Callable:
+        return lambda x: sin(x)
 
 
 class SimpsonIntegration:
 
     @staticmethod
     def inf_interpolation(test: Test):
-        return lambda x: test.expression()((x / (1 - x)) / ((1 - x) ** 2))
+        return lambda x: test.expression()((x / (1 - x))) / ((1 - x) ** 2)
 
     # For a regular grid
     @staticmethod
@@ -58,7 +66,7 @@ class SimpsonIntegration:
         a, b = test.bounds
         return (a / (1 - a), 1 - ACCURACY), self.inf_interpolation(test)
 
-    def integrate(self, nodes_amount: int, test: Test):
+    def integrate(self, test: Test, nodes_amount: int):
         a, b = test.bounds
         expr = test.expression()
 
@@ -77,16 +85,55 @@ class SimpsonIntegration:
         return (h / 3) * result
 
 
-def get_approx_order(test: Test):
-    """TODO: impl :)"""
+def get_approx_order(test: Test, N: int, r: int, print_flag: bool = False):
+    a, b = test.bounds
+    integrate = SimpsonIntegration().integrate
+    INTEGRAL = scipy.integrate.quad(test.expression(), a, b)[0]
+
+    h_p: float = abs(integrate(test, N // r) - INTEGRAL)
+    h_p = h_p if h_p != 0 else np.finfo(float).eps
+    _2h_p: float = abs(integrate(test, N) - INTEGRAL)
+    _2h_p = _2h_p if _2h_p != 0 else np.finfo(float).eps
+
+    if print_flag:
+        print(f"h:{h_p} ")
+        print(f"2h: {_2h_p}")
+
+    return (abs(log(h_p / _2h_p, r)), h_p, _2h_p)
+
+
+def nodes_amount_correlation(test: Test, N: int = 100, r: int = 2):
+    STEPS = 100
+    nodes_amount = [N * i for i in range(1, STEPS)]
+    approxes = [get_approx_order(test, N * i, r) for i in range(1, STEPS)]
+
+    orders = [approx[0] for approx in approxes]
+    h_p = [approx[1] for approx in approxes]
+    _2h_p = [approx[2] for approx in approxes]
+
+    fig, ax = plt.subplots()
+
+    ax.plot(nodes_amount, h_p)
+    ax.plot(nodes_amount, orders)
+    ax.plot(nodes_amount, _2h_p)
+
+    plt.title(f"Step = {N}, r = {r}")
+    plt.xlabel("Nodes amount")
+    plt.ylabel("Approximation order")
+    # plt.show()
 
 
 def run_test(test: Test):
     i = SimpsonIntegration()
-    actual = i.integrate(100000, test)
-    expected = integrate.quad(test.expression(), test.bounds[0], test.bounds[1])[0]
-    print(f"Actual: {actual}\nExpected: {expected}\nApproximation order: ")
-    assert abs(expected - actual) < ACCURACY
+    a, b = test.bounds
+
+    ACTUAL = i.integrate(test, test.nodes_amount)
+    EXPECTED = scipy.integrate.quad(test.expression(), a, b)[0]
+    nodes_amount_correlation(test)
+    print(
+        f"Actual: {ACTUAL}\nExpected: {EXPECTED}\nApproximation order: {get_approx_order(test, test.nodes_amount, r=2, print_flag=True)[0]}")
+
+    print(abs(EXPECTED - ACTUAL))
 
 
 if __name__ == "__main__":
@@ -97,3 +144,7 @@ if __name__ == "__main__":
     if SECOND_TEST:
         print("-------------- Running 2nd test ----------------")
         run_test(SecondTest())
+
+    if THIRD_TEST:
+        print("-------------- Running 3rd test ----------------")
+        run_test(ThirdTest())
