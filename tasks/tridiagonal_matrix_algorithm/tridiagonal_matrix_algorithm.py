0446a44e7dc67c99b0dbc1c81406e629b8b19ca6
MahasApu <maryvask0@yandex.ru>
2024-05-01 15:55:41 +0700

    add graphics of maximum derivation

--- tasks/tridiagonal_matrix_algorithm/tridiagonal_matrix_algorithm.py
+++ tasks/tridiagonal_matrix_algorithm/tridiagonal_matrix_algorithm.py
@@ -1,7 +1,8 @@
-from typing import List
+from typing import List, Callable
 from math import cos, pi
 
 import matplotlib.pyplot as plt
+
 
 # ссылка на методичку: https://eqworld.ipmnet.ru/ru/library/books/Knyazeva_progonka_2006ru.pdf
 # Правая прогонка
@@ -28,6 +29,24 @@ def tridiagonal_method(A: List[float], C: List[float], B: List[float], F: List[f
     for i in range(n - 1, 0, -1):
         y[i - 1] = alpha[i] * y[i] + beta[i]
     return y
+
+
+def get_matrices(func: Callable, left: list, right: list, bounds: tuple, nodes_amount: int):
+    l, r = bounds
+    h = (r - l) / nodes_amount
+    A: List[float] = [1 / h ** 2 if _ != 0 else 0 for _ in range(0, nodes_amount)]
+    B: List[float] = [-2 / h ** 2 for _ in range(1, nodes_amount)]
+    C: List[float] = [1 / h ** 2 if _ != nodes_amount else 0 for _ in range(1, nodes_amount + 1)]
+    X: List[float] = [l + i * h for i in range(1, nodes_amount)]
+    F: List[float] = [func(x_i) for x_i in X]
+
+    A = A + [right[0]]
+    B = [left[0]] + B + [right[1]]
+    C = [left[1]] + C
+    F = [left[2]] + F + [right[2]]
+    X = [l] + X + [r]
+
+    return A, B, C, F, X
 
 
 def get_bounds(h: float) -> (list, list):
@@ -64,40 +83,96 @@ def get_bounds(h: float) -> (list, list):
     return get_left(), get_right()
 
 
-if __name__ == "__main__":
-    NODES_AMOUNT = 20
-    BOUNDS = (-pi / 2, pi / 2)
+def examples(option: int, h: float):
+    assert option in [1, 2, 3]
 
-    l, r = BOUNDS
-    h = (r - l) / NODES_AMOUNT
+    # y'(-pi/2) = 3
+    # y(pi/2) = 4
+    if option == 1:
+        return [-1 / h, 1 / h, 3], [0, 1, 4]
 
+    # y(-pi/2) = 0.5
+    # y(pi/2) = 5.2
+    elif option == 2:
+        return [1, 0, 0.5], [0, 1, 5.2]
+
+    # y(-pi/2) = -0.8
+    # y'(pi/2) = -0.1
+    else:
+        return [1, 0, -0.8], [1 / h, -1 / h, -0.1]
+
+
+# Функция для отображения максимального отклонения численного решения от точного.
+# Итерация по количеству узлов (с шагом 500).
+# Граничные условия берутся из функции examples().
+def plot_max_deviation(option: int, bounds: tuple, func: Callable):
+    assert option in [1, 2, 3]
+
+    l, r = bounds
+    STEPS = [100 * x for x in range(1, 10)]
+    deviation = []
+
+    X, Y, Y_exact = [], [], []
+
+    for amount in STEPS:
+        h = (r - l) / amount
+        left, right = examples(option, h)
+        A, B, C, F, X = get_matrices(func, left, right, bounds, amount)
+        Y = tridiagonal_method(A, B, C, F)
+        m1 = left[2]
+        m2 = right[2]
+        c1 = m1 + 1
+        c2 = m2 - c1 * pi / 2
+        Y_exact = [(-cos(x) + c1 * x + c2) for x in X]
+        deviation.append(max([abs(Y[i] - Y_exact[i]) for i in range(len(Y))]))
+
+    # plot
+    fig, axs = plt.subplots(2, 1, figsize=(5, 6), tight_layout=True)
+    axs[0].grid(True)
+    axs[1].grid(True)
+
+    axs[0].plot(X, Y, label="Численное")
+    axs[0].plot(X, Y_exact, label="Точное")
+
+    axs[1].plot(STEPS, deviation, label="Максимальное отклонение")
+
+    axs[0].legend()
+    axs[1].legend()
+    plt.show()
+
+
+# Функция для построения графиков точного и численного решений.
+# Граничные условия задаются произвольно с помощью функции get_bounds()
+def plot_solution_with_set_conditions(bounds: tuple, amount: int, func: Callable):
+    l, r = bounds
+    h = (r - l) / amount
     left, right = get_bounds(h)
-
-    func = lambda x: cos(x)
-
-    A: List[float] = [1 / h ** 2 if _ != 0 else 0 for _ in range(0, NODES_AMOUNT)]
-    B: List[float] = [-2 / h ** 2 for _ in range(1, NODES_AMOUNT)]
-    C: List[float] = [1 / h ** 2 if _ != NODES_AMOUNT else 0 for _ in range(1, NODES_AMOUNT + 1)]
-    X: List[float] = [l + i * h for i in range(1, NODES_AMOUNT)]
-    F: List[float] = [func(x_i) for x_i in X]
-
-    A = A + [right[0]]
-    B = [left[0]] + B + [right[1]]
-    C = [left[1]] + C
-    F = [left[2]] + F + [right[2]]
-    X = [l] + X + [r]
-
+    A, B, C, F, X = get_matrices(func, left, right, bounds, amount)
     Y = tridiagonal_method(A, B, C, F)
 
-    fig, ax = plt.subplots()
-    ax.plot(X, Y, label="Численное")
-
-    # exact solution
     m1 = left[2]
     m2 = right[2]
     c1 = m1 + 1
     c2 = m2 - c1 * pi / 2
-    ax.plot(X, [(-cos(x) + c1 * x + c2) for x in X], label="Точное")
+    Y_exact = [(-cos(x) + c1 * x + c2) for x in X]
+
+    # plot
+    fig, ax = plt.subplots()
     ax.grid(True)
-    plt.legend()
+
+    ax.plot(X, Y, label="Численное")
+    ax.plot(X, Y_exact, label="Точное")
+
+    ax.legend()
     plt.show()
+
+
+if __name__ == "__main__":
+    BOUNDS = (-pi / 2, pi / 2)
+    NODES_AMOUNT = 100
+    FUNC = lambda x: cos(x)
+
+    # для выбора примера (1, 2, 3)
+    OPTION = 1
+    plot_max_deviation(OPTION, BOUNDS, FUNC)
+    # plot_solution_with_set_conditions(BOUNDS, NODES_AMOUNT, FUNC)
